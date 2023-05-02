c144d6f7af169916b51570ceec1e58faacf2031f
jhed-98 <cod.mobile.jhed@gmail.com>
2023-05-01 21:05:40 -0500

    first commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/main.css";
+
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(VueAxios, axios);
+
+app.mount("#app");
